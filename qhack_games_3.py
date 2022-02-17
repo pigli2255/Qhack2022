2035edb6f97884e6e79f96113f5bcea9ec2242f7
pigli2255 <56066423+pigli2255@users.noreply.github.com>
2022-02-17 16:23:15 +0100

    Solution Challange Game 3

---
+++ qhack_games_3.py
@@ -0,0 +1,89 @@
+#! /usr/bin/python3
+
+import sys
+import pennylane as qml
+from pennylane import numpy as np
+
+dev = qml.device("default.qubit", wires=1, shots=1)
+
+
+@qml.qnode(dev)
+def is_bomb(angle):
+    """Construct a circuit that implements a one shot measurement at the bomb.
+
+    Args:
+        - angle (float): transmissivity of the Beam splitter, corresponding
+        to a rotation around the Y axis.
+
+    Returns:
+        - (np.ndarray): a length-1 array representing result of the one-shot measurement
+    """
+
+    # QHACK #
+    qml.RY(2*angle, wires=0)
+
+    # QHACK #
+
+    return qml.sample(qml.PauliZ(0))
+
+
+@qml.qnode(dev)
+def bomb_tester(angle):
+    """Construct a circuit that implements a final one-shot measurement, given that the bomb does not explode
+
+    Args:
+        - angle (float): transmissivity of the Beam splitter right before the final detectors
+
+    Returns:
+        - (np.ndarray): a length-1 array representing result of the one-shot measurement
+    """
+
+    # QHACK #
+    #qml.PauliX(0)
+    qml.RY(2*angle,wires=0)
+
+    # QHACK #
+
+    return qml.sample(qml.PauliZ(0))
+
+
+def simulate(angle, n):
+    """Concatenate n bomb circuits and a final measurement, and return the results of 10000 one-shot measurements
+
+    Args:
+        - angle (float): transmissivity of all the beam splitters, taken to be identical.
+        - n (int): number of bomb circuits concatenated
+
+    Returns:
+        - (float): number of bombs successfully tested / number of bombs that didn't explode.
+    """
+
+    # QHACK #
+    counter = 0
+    num_D = 0
+
+    for shots in range(10000):
+        for i in range(n):
+            val = is_bomb(angle)
+            if val == 1:
+                break
+            if i == n-1 and val != 1:
+                counter += 1
+                bomb = bomb_tester(angle)
+                if bomb==-1:
+                    num_D += 1
+
+    r = num_D/counter
+
+    return r
+
+
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    output = simulate(float(inputs[0]), int(inputs[1]))
+    print(f"{output}")
